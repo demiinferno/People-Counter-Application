d6e25e7ad16a0e3e74ea01f48759ba48f2818f88
demiinferno <78436453+demiinferno@users.noreply.github.com>
2021-02-03 19:45:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,7 @@
  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
-import os
+import react 
 import sys
 import time
 import socket
